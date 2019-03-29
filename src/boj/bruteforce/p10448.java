a8f51049c82520951fc1190f32328214a44f084f
baelanche <baelanche@naver.com>
2019-03-29 22:22:31 +0900

    20190329

---
+++ src/boj/bruteforce/p10448.java
@@ -0,0 +1,34 @@
+package boj.bruteforce;
+
+import java.util.Scanner;
+
+public class p10448 {
+	
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        boolean eq = false;
+        
+        while(t-->0){
+            int x = sc.nextInt();
+            
+            for(int i=1; i<=43; i++) {
+                for(int j=1; j<=43; j++) {
+                    for(int k=1; k<=43; k++) {
+                        int sum = (i*(i+1)/2) + (j*(j+1)/2) + (k*(k+1)/2);
+                        if(sum == x){
+                            eq = true;
+                            break;
+                        }
+                    }
+                    if(eq) break;
+                }
+                if(eq) break;
+            }
+            if(eq) System.out.println(1);
+            else System.out.println(0);
+            eq = false;
+        }
+    }
+}
---
+++ src/boj/bruteforce/p2503.java
@@ -0,0 +1,58 @@
+package boj.bruteforce;
+
+import java.util.Scanner;
+
+public class p2503 {
+
+	public static String[] base;
+    public static String[] s;
+    public static String[] b;
+    public static int cnt = 0;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        base = new String[t];
+        s = new String[t];
+        b = new String[t];
+        
+        for(int i=0; i<t; i++) {
+            base[i] = sc.next();
+            s[i] = sc.next();
+            b[i] = sc.next();
+        }
+        
+        for(int i=123; i<=987; i++) {
+            check(String.valueOf(i));
+        }
+        
+        System.out.println(cnt);
+    }
+    
+    public static void check(String arr) {
+        char[] charArr = arr.toCharArray();
+        int eq = 0;
+        
+        for(int i=0; i<base.length; i++) {
+            if(charArr[0] == charArr[1] || charArr[0] == charArr[2] || charArr[1] == charArr[2] ||
+              charArr[0] == '0' || charArr[1] == '0' || charArr[2] == '0')
+                continue;
+            int str = 0;
+            int bal = 0;
+            for(int j=0; j<3; j++) {
+                for(int k=0; k<3; k++) {
+                    if(charArr[j] == base[i].charAt(k)) {
+                        if(j == k) str++;
+                        else bal++;
+                    }
+                }
+            }
+            if(str == Integer.parseInt(s[i]) && bal == Integer.parseInt(b[i]))
+                eq++;
+        }
+        if(eq == base.length)
+                cnt++;
+    }
+}
