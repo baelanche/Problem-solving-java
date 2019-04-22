b4a7b23393a458475333ac8ea7f60c7b78332e1a
baelanche <baelanche@naver.com>
2019-04-22 21:29:07 +0900

    20190422

--- src/boj/dp/prob/p2749.java
+++ src/boj/dp/prob/p2749.java
@@ -1,31 +1,28 @@
 package boj.dp.prob;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class p2749 {
 
-	public static int dp[];
-	
-	public static int f(int n) {
-		if(n == 0) return 0;
-		if(n == 1) return 1;
-		if(dp[n] != -1) return dp[n];
-		
-		int result = f(n-1)%1000000 + f(n-2)%1000000;
-		dp[n] = result;
-		return result;
-	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		//int n = sc.nextLong();
-		dp = new int[100000000];
+		long n = sc.nextLong();
 		
-		Arrays.fill(dp, -1);
-		//System.out.print(f(n)%1000000);
+		int p = 1500000;
+		
+		long dp[] = new long[p];
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i=2; i<p; i++) {
+			if(n < i) break;
+			dp[i] = dp[i-1] + dp[i-2];
+			dp[i] %= (long)Math.pow(10, 6);
+		}
 
+		if(n >= p) n %= p;
+		System.out.println(dp[(int)n]);
 	}
 
 }
