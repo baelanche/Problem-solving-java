255f2e94dc37f1950503a61880d0b8bf9c1b2758
baelanche <baelanche@naver.com>
2019-08-22 21:25:25 +0900

    20190822

---
+++ src/boj/realize/prob/p5337.java
@@ -0,0 +1,10 @@
+package boj.realize.prob;
+
+public class p5337 {
+
+	public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
