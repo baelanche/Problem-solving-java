d633e1b55880ec9282da989fd544776e8417fe85
baegyu <baelanche@naver.com>
2022-02-16 11:01:44 +0900

    p18108

---
+++ src/boj/realize/p18108.java
@@ -0,0 +1,13 @@
+package boj.realize;
+
+import java.util.*;
+
+public class p18108 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int y = sc.nextInt();
+        System.out.println(y-543);
+    }
+}
