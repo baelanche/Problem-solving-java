831bf9a7fc2843dce77535ae8247d6171796d704
baelanche <baelanche@naver.com>
2022-05-23 19:12:34 +0900

    p25212

--- src/icpc/inha/G.java
+++ src/icpc/inha/G.java
@@ -8,28 +8,25 @@ import java.util.StringTokenizer;
 public class G {
 
 	static int n;
-	static float cake[];
+	static double cake[];
 	static int ans = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 		n = Integer.parseInt(br.readLine());
-		cake = new float[n];
+		cake = new double[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			cake[i] = 1.0f/Float.parseFloat(st.nextToken());
-		}
+		for(int i=0; i<n; i++)
+			cake[i] = 1.0d/Double.parseDouble(st.nextToken());
 		
-		dfs(0, 0.0f);
+		dfs(0, 0.0d);
 		System.out.println(ans);
 	}
 	
-	public static void dfs(int idx, float sum) {
-		if(sum >= 99.0f/100.0f && sum <= 101.0f/100.0f) {
+	public static void dfs(int idx, double sum) {
+		if(sum >= 99.0f/100.0d && sum <= 101.0f/100.0d) {
 			ans++;
-			for(int i=idx; i<n; i++)
-				dfs(idx+1, sum+cake[idx]);
 			return;
 		}
 		if(idx == n) return;
