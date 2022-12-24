f936ecbc4662a96f8df9e6a615c4a399604a5a5b
baegyu <baelanche@naver.com>
2022-12-24 16:25:12 +0900

    Update p2630.java

--- src/boj/divide/prob/p2630.java
+++ src/boj/divide/prob/p2630.java
@@ -3,42 +3,58 @@ package boj.divide.prob;
 import java.util.Scanner;
 
 public class p2630 {
-
-	static int a[][];
-    static int white = 0;
-    static int blue = 0;
     
-    public static void main(String[] args) {
+    public static int a[][];
+    public static int white = 0;
+    public static int blue = 0;
+    
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
         a = new int[n][n];
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
                 a[i][j] = sc.nextInt();
+            }
+        }
         
         divide(0, 0, n);
         System.out.println(white);
         System.out.println(blue);
     }
     
-    public static void divide(int x, int y, int n) {
-        if(n == 0) return;
+    public static void divide(int r, int c, int len) {
+        if (len < 1) {
+            return;
+        }
         
-        int color = a[x][y];
-        for(int i=x; i<x+n; i++) {
-            for(int j=y; j<y+n; j++) {
-                if(color != a[i][j]) {
-                    divide(x, y, n/2);
-                    divide(x, y + n/2, n/2);
-                    divide(x + n/2, y, n/2);
-                    divide(x + n/2, y + n/2, n/2);
-                    return;
+        boolean b = check(a[r][c], r, c, len);
+        
+        if (b) {
+            if (a[r][c] == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+            return;
+        }
+        
+        divide(r, c, len/2);
+        divide(r, c + len/2, len/2);
+        divide(r + len/2, c, len/2);
+        divide(r + len/2, c + len/2, len/2);
+    }
+    
+    public static boolean check(int init, int r, int c, int len) {
+        for (int i=r; i<r+len; i++) {
+            for (int j=c; j<c+len; j++) {
+                if (init != a[i][j]) {
+                    return false;
                 }
             }
         }
-        
-        if(color == 0) white++;
-        else blue++;
+        return true;
     }
 }
