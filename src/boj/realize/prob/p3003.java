3853e9959a639b839f3f4af343394f4f6562e9e5
baegyu <baelanche@naver.com>
2023-01-01 21:29:31 +0900

    Update p3003.java

--- src/boj/realize/prob/p3003.java
+++ src/boj/realize/prob/p3003.java
@@ -1,19 +1,18 @@
 package boj.realize.prob;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class p3003 {
-
-	public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         
-        int king = 1 - sc.nextInt();
-        int queen = 1 - sc.nextInt();
-        int rook = 2 - sc.nextInt();
-        int bishop = 2 - sc.nextInt();
-        int knight = 2 - sc.nextInt();
-        int pawn = 8 - sc.nextInt();
+        int king = sc.nextInt();
+        int queen = sc.nextInt();
+        int rook = sc.nextInt();
+        int bishop = sc.nextInt();
+        int knight = sc.nextInt();
+        int pawn = sc.nextInt();
         
-        System.out.println(king + " " + queen + " " + rook + " " + bishop + " " + knight + " " + pawn);
+        System.out.println((1 - king) + " " + (1 - queen) + " " + (2 - rook) + " " + (2 - bishop) + " " + (2 - knight) + " " + (8 - pawn));
     }
 }
