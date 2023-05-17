777aca5271d5e6e71770bd8f3b4a93897a4be922
unknown <baelanche@naver.com>
2023-05-17 22:21:30 +0900

    solve dp

--- src/boj/dp/prob/p28017.java
+++ src/boj/dp/prob/p28017.java
@@ -1,43 +1,38 @@
+package boj.dp.prob;
+
 import java.util.*;
 
-public class Main {
-    
-    public static int n;
-    public static int m;
-    public static int a[][];
-    public static Map<Integer, Integer> selected;
-    public static int answer = Integer.MAX_VALUE;
-    
+public class p28017 {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         
-        n = sc.nextInt();
-        m = sc.nextInt();
-        a = new int[500][500];
-        selected = new HashMap<Integer, Integer>();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[][] = new int[501][501];
+        int dp[][] = new int[501][501];
         
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
                 a[i][j] = sc.nextInt();
             }
-        }
-        
-        bf(0, 0);
-        System.out.println(answer);
-    }
-    
-    public static void bf(int cnt, int sum) {
-        if (cnt == n) {
-            answer = Math.min(answer, sum);
-            return;
-        }
-        
-        for (int i=0; i<m; i++) {
-            if (selected.get(i) == null) {
-                selected.put(i, i);
-                bf(cnt + 1, sum + a[cnt][i]);
-                selected.remove(i);
+            for (int j=1; j<=m; j++) {
+                int min = 10001;
+                for (int k=1; k<=m; k++) {
+                    if (j == k) {
+                        continue;
+                    }
+                    min = Math.min(min, dp[i-1][k]);
+                }
+                dp[i][j] = a[i][j] + min;
             }
         }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i=1; i<=m; i++) {
+            answer = Math.min(answer, dp[n][i]);
+        }
+
+        System.out.println(answer);
     }
 }
