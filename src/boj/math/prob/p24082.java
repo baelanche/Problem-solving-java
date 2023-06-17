719bfd70aabadc9b66da9b325e4fc7739bcc2231
unknown <baelanche@naver.com>
2023-06-17 10:23:51 +0900

    p24082

---
+++ src/boj/math/prob/p24082.java
@@ -0,0 +1,13 @@
+package boj.math.prob;
+
+import java.util.Scanner;
+
+public class p24082 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        System.out.println(x * x * x);
+    }
+}
