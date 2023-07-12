90e19953abd5728bc3bfc3932db4fa96e73c8bf2
baegyu <baelanche@naver.com>
2023-07-13 08:05:54 +0900

    Create p24265.java

---
+++ src/boj/math/prob/p24265.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class p24265 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextInt();
+        
+        System.out.println(n * (n - 1) / 2);
+        System.out.println(2);
+    }
+}
