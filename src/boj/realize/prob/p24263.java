f83af937bcfd1c5ed2d9ec9a2e45e3ed6dd91a31
unknown <baelanche@naver.com>
2023-07-13 05:41:32 +0900

    p24263

---
+++ src/boj/realize/prob/p24263.java
@@ -0,0 +1,15 @@
+package boj.realize.prob;
+
+import java.util.Scanner;
+
+public class p24263 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
