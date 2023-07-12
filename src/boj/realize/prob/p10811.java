a991d677fe0929e2577451dc3535ec13d4a2ab7a
baegyu <baelanche@naver.com>
2023-07-12 14:23:53 +0900

    Create p10811.java

---
+++ src/boj/realize/prob/p10811.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class p10811 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[] = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            a[i] = i + 1;
+        }
+        
+        for (int k=0; k<m; k++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            while (i < j) {
+                int temp = a[i-1];
+                a[i-1] = a[j-1];
+                a[j-1] = temp;
+                i++;
+                j--;
+            }
+        }
+        
+        Arrays.stream(a).forEach(x -> System.out.print(x + " "));
+    }
+}
