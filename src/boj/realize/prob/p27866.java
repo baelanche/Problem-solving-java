e67c1f2b7eaa32572f32280662f0d6bb7b1f96c3
baegyu <baelanche@naver.com>
2023-07-12 14:34:22 +0900

    Create p27866.java

---
+++ src/boj/realize/prob/p27866.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class p27866 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        System.out.println(s.charAt(sc.nextInt() - 1));
+    }
+}
