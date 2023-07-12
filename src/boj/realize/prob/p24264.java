6924f67a2082ff58b2909859d77bd7632c87c14d
unknown <baelanche@naver.com>
2023-07-13 05:54:16 +0900

    p24264

---
+++ src/boj/realize/prob/p24264.java
@@ -0,0 +1,15 @@
+package boj.realize.prob;
+
+import java.util.Scanner;
+
+public class p24264 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextInt();
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
