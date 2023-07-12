d0724dc05d06d08440a9a0a8b1600bc794f63d41
baegyu <baelanche@naver.com>
2023-07-13 08:07:24 +0900

    Create p24266.java

---
+++ src/boj/math/prob/p24266.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class p24266 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextInt();
+        
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
